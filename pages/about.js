4d40540f720cddd46d2fbaec3c74d933da226faa
eman-aproductfit <eman.aproductfit@gmail.com>
2022-05-07 12:06:42 +0800

    Navigation
    Added 2 pages
    Home and Navigations
    clickable Nav both pages
    Added customed navigation styles

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div  >
+<Head>
+    <title>Slick About</title>
+    <meta name='keywords' content='web'/>
+</Head>
+
+
+       <h1>Welcome to next about</h1>
+    </div>
+  )
+}
